7202244e769c4caf6a5314d0f92708215e798de2
skytnt <SkyTNT@outlook.com>
2024-09-23 01:34:20 +0800

    Update export.py

--- export.py
+++ export.py
@@ -31,7 +31,7 @@ def export_onnx(model, model_inputs, input_names, output_names, dynamic_axes, pa
                       model_inputs,  # model input (or a tuple for multiple inputs)
                       path,  # where to save the model (can be a file or file-like object)
                       export_params=True,  # store the trained parameter weights inside the model file
-                      opset_version=11,  # the ONNX version to export the model to
+                      opset_version=14,  # the ONNX version to export the model to
                       do_constant_folding=True,  # whether to execute constant folding for optimization
                       input_names=input_names,  # the model's input names
                       output_names=output_names,  # the model's output names
